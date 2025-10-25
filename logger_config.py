7ec611ceb8740893e5705abb783b59606fb94445
Chris Courville <ccourville80@gmail.com>
2025-10-25 19:07:21 -0400

    added sql and html

--- logger_config.py
+++ logger_config.py
@@ -1,4 +1,36 @@
-# logger_config.py
+# # logger_config.py
+# import logging
+# from logging.handlers import RotatingFileHandler
+# import os
+
+# LOG_DIR = "logs"
+# LOG_FILE = "scooter_app.log"
+# os.makedirs(LOG_DIR, exist_ok=True)
+# log_path = os.path.join(LOG_DIR, LOG_FILE)
+
+# def get_logger(name="scooter_app"):
+#     logger = logging.getLogger(name)
+#     if logger.handlers:
+#         return logger  # already configured
+
+#     logger.setLevel(logging.DEBUG)
+
+#     # console handler
+#     ch = logging.StreamHandler()
+#     ch.setLevel(logging.DEBUG)
+#     formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+#     ch.setFormatter(formatter)
+#     logger.addHandler(ch)
+
+#     # rotating file handler
+#     fh = RotatingFileHandler(log_path, maxBytes=5 * 1024 * 1024, backupCount=5, encoding='utf-8')
+#     fh.setLevel(logging.DEBUG)
+#     fh.setFormatter(formatter)
+#     logger.addHandler(fh)
+
+#     return logger
+
+
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -11,22 +43,21 @@ log_path = os.path.join(LOG_DIR, LOG_FILE)
 def get_logger(name="scooter_app"):
     logger = logging.getLogger(name)
     if logger.handlers:
-        return logger  # already configured
-
+        return logger
     logger.setLevel(logging.DEBUG)
 
-    # console handler
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    # rotating file handler
     fh = RotatingFileHandler(log_path, maxBytes=5 * 1024 * 1024, backupCount=5, encoding='utf-8')
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
     return logger
+
 
